d36848b342e4c6a470457b7b969a5d2acea0512c
SandeepPandey <dextersandeep@gmail.com>
2022-07-20 11:03:07 +0530

    commit12

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -3,6 +3,7 @@ import { Post } from "../post.model";
 import { Subscription } from "rxjs";
 import { PostService } from "../post.service";
 import { PageEvent } from "@angular/material/paginator";
+import { AuthService } from "src/app/auth/auth.service";
 
 @Component({
   selector:'app-post-list',
@@ -24,8 +25,9 @@ posts: Post[]= Array();
   postPerPage=2;
   currentPage=1;
   pageSizeOptions=[1,2,5,10];
+  private authStatusSub:Subscription= new Subscription;
 
-constructor(public postService: PostService){}
+constructor(public postService: PostService , private authService: AuthService){}
 ngOnInit(){
   this.isLoading= true;
      this.postService.getPosts(this.postPerPage, this.currentPage);
@@ -35,7 +37,8 @@ ngOnInit(){
     this.totalPosts=postData.postCount;
      this.posts= postData.posts;
     });
-}
+   this.authService.
+  }
 
 
 onChangePage(pageData:PageEvent){
