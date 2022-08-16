cc448cc28d580d4398c4233db3db3bef4f8a27f1
SandeepPandey <dextersandeep@gmail.com>
2022-08-16 12:45:49 +0530

    Deployment

--- backend/app.js
+++ backend/app.js
@@ -40,7 +40,7 @@ app.use((req,res,next)=>{
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use("/images", express.static(path.join("backend/images")));
+app.use("images", express.static(path.join("backend/images")));
 
 app.use("/api/user",userRoutes);
 app.use("/api/posts",postRoutes);
